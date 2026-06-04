81e5e3d4ead2c25ba40770af000ca67232483b1f
mkurkar <kingkurkar@gmail.com>
2026-06-04 05:57:22 +0300

    feat: customize default Header (DesktopHeader) with KKUx design
    - Replaced site-header-desktop/mobile with unified kkux-header
    - KKUx-styled nav links, user dropdown, ghost buttons
    - Arabic/English bilingual messages for all header text
    - Dropdown: rounded-lg, shadow-lg, bordered, hover:bg-gray-100
    - Added nav--main, nav--right, user-icon, username, chevron styles
    - IBM Plex Sans Arabic fonts throughout

--- src/Header.messages.jsx
+++ src/Header.messages.jsx
@@ -79,7 +79,7 @@ const messages = defineMessages({
   'header.label.account.menu.for': {
     id: 'header.label.account.menu.for',
     defaultMessage: 'Account menu for {username}',
-    description: 'The aria label for the account menu trigger when the username is displayed in it',
+    description: 'The aria label for the account menu trigger',
   },
   'header.label.main.nav': {
     id: 'header.label.main.nav',
@@ -99,12 +99,12 @@ const messages = defineMessages({
   'header.label.secondary.nav': {
     id: 'header.label.secondary.nav',
     defaultMessage: 'Secondary',
-    description: 'The aria label for the seconary nav',
+    description: 'The aria label for the secondary nav',
   },
   'header.label.skip.nav': {
     id: 'header.label.skip.nav',
     defaultMessage: 'Skip to main content',
-    description: 'A link used by screen readers to allow users to skip to the main content of the page.',
+    description: 'Screen reader skip link',
   },
   'header.label.app.nav': {
     id: 'header.label.app.nav',
@@ -113,4 +113,30 @@ const messages = defineMessages({
   },
 });
 
+const arMessages = {
+  'header.links.courses': '\u0627\u0644\u0645\u0633\u0627\u0642\u0627\u062a',
+  'header.links.programs': '\u0627\u0644\u0628\u0631\u0627\u0645\u062c',
+  'header.links.content.search': '\u0627\u0643\u062a\u0634\u0641 \u0627\u0644\u062c\u062f\u064a\u062f',
+  'header.links.schools': '\u0627\u0644\u0645\u062f\u0627\u0631\u0633 \u0648\u0627\u0644\u0634\u0631\u0643\u0627\u0621',
+  'header.user.menu.dashboard': '\u0644\u0648\u062d\u0629 \u0627\u0644\u062a\u062d\u0643\u0645',
+  'header.user.menu.profile': '\u0627\u0644\u0645\u0644\u0641 \u0627\u0644\u0634\u062e\u0635\u064a',
+  'header.user.menu.account.settings': '\u0627\u0644\u062d\u0633\u0627\u0628',
+  'header.user.menu.order.history': '\u0633\u062c\u0644 \u0627\u0644\u0637\u0644\u0628\u0627\u062a',
+  'header.user.menu.logout': '\u062a\u0633\u062c\u064a\u0644 \u0627\u0644\u062e\u0631\u0648\u062c',
+  'header.user.menu.login': '\u062a\u0633\u062c\u064a\u0644 \u0627\u0644\u062f\u062e\u0648\u0644',
+  'header.user.menu.register': '\u062a\u0633\u062c\u064a\u0644',
+  'header.user.menu.studio.home': '\u0627\u0644\u0635\u0641\u062d\u0629 \u0627\u0644\u0631\u0626\u064a\u0633\u064a\u0629',
+  'header.user.menu.studio.maintenance': '\u0627\u0644\u0635\u064a\u0627\u0646\u0629',
+  'header.label.account.nav': '\u0627\u0644\u062d\u0633\u0627\u0628',
+  'header.label.account.menu': '\u0642\u0627\u0626\u0645\u0629 \u0627\u0644\u062d\u0633\u0627\u0628',
+  'header.label.account.menu.for': '\u0642\u0627\u0626\u0645\u0629 \u0627\u0644\u062d\u0633\u0627\u0628 \u0644\u0640 {username}',
+  'header.label.main.nav': '\u0631\u0626\u064a\u0633\u064a\u0629',
+  'header.label.main.menu': '\u0627\u0644\u0642\u0627\u0626\u0645\u0629 \u0627\u0644\u0631\u0626\u064a\u0633\u064a\u0629',
+  'header.label.main.header': '\u0631\u0626\u064a\u0633\u064a',
+  'header.label.secondary.nav': '\u062b\u0627\u0646\u0648\u064a\u0629',
+  'header.label.skip.nav': '\u062a\u062e\u0637\u0649 \u0625\u0644\u0649 \u0627\u0644\u0645\u062d\u062a\u0648\u0649 \u0627\u0644\u0631\u0626\u064a\u0633\u064a',
+  'header.label.app.nav': '\u0627\u0644\u062a\u0637\u0628\u064a\u0642',
+};
+
+export { arMessages };
 export default messages;
--- src/desktop-header/DesktopHeader.jsx
+++ src/desktop-header/DesktopHeader.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { injectIntl, intlShape } from '@edx/frontend-platform/i18n';
+import { useIntl } from '@edx/frontend-platform/i18n';
 import { getConfig } from '@edx/frontend-platform';
 
-// Local Components
 import { Menu, MenuTrigger, MenuContent } from '../Menu';
 import Avatar from '../Avatar';
 import LogoSlot from '../plugin-slots/LogoSlot';
@@ -15,100 +14,81 @@ import DesktopSecondaryMenuSlot from '../plugin-slots/DesktopSecondaryMenuSlot';
 import DesktopUserMenuSlot from '../plugin-slots/DesktopUserMenuSlot';
 import { desktopUserMenuDataShape } from './DesktopHeaderUserMenu';
 
-// i18n
-import messages from '../Header.messages';
-
-// Assets
+import messages, { arMessages } from '../Header.messages';
 import { CaretIcon } from '../Icons';
 
-class DesktopHeader extends React.Component {
-  constructor(props) { // eslint-disable-line no-useless-constructor
-    super(props);
-  }
+const DesktopHeader = (props) => {
+  const {
+    mainMenu, secondaryMenu, userMenu, loggedOutItems,
+    logo, logoAltText, logoDestination,
+    avatar, username, loggedIn,
+  } = props;
+  const intl = useIntl();
+  const isArabic = intl.locale && intl.locale.startsWith('ar')
+    || (typeof document !== 'undefined' && document.documentElement.dir === 'rtl');
 
-  renderMainMenu() {
-    const { mainMenu } = this.props;
-    return <DesktopMainMenuSlot menu={mainMenu} />;
-  }
+  const t = (msg, values) => {
+    if (isArabic && arMessages[msg.id]) {
+      let text = arMessages[msg.id];
+      if (values) {
+        Object.keys(values).forEach((key) => { text = text.replace(`{${key}}`, values[key]); });
+      }
+      return text;
+    }
+    return intl.formatMessage(msg, values);
+  };
 
-  renderSecondaryMenu() {
-    const { secondaryMenu } = this.props;
-    return <DesktopSecondaryMenuSlot menu={secondaryMenu} />;
-  }
+  const logoProps = { src: logo, alt: logoAltText, href: logoDestination };
+  const logoClasses = getConfig().AUTHN_MINIMAL_HEADER ? 'mw-100' : null;
 
-  renderUserMenu() {
-    const {
-      userMenu,
-      avatar,
-      username,
-      intl,
-    } = this.props;
+  return (
+    <header className="kkux-header">
+      <a className="nav-skip sr-only sr-only-focusable" href="#main">
+        {t(messages['header.label.skip.nav'])}
+      </a>
+      <div className={`kkux-header__inner ${logoClasses}`}>
+        {/* Logo */}
+        <LogoSlot {...logoProps} />
 
-    return (
-      <Menu transitionClassName="menu-dropdown" transitionTimeout={250}>
-        <MenuTrigger
-          tag="button"
-          aria-label={intl.formatMessage(messages['header.label.account.menu.for'], { username })}
-          className="btn btn-outline-primary d-inline-flex align-items-center pl-2 pr-3"
-        >
-          <Avatar size="1.5em" src={avatar} alt="" className="mr-2" />
-          {username} <CaretIcon role="img" aria-hidden focusable="false" />
-        </MenuTrigger>
-        <MenuContent className="mb-0 dropdown-menu show dropdown-menu-right pin-right shadow py-2">
-          <DesktopUserMenuSlot menu={userMenu} />
-        </MenuContent>
-      </Menu>
-    );
-  }
+        {/* Main navigation */}
+        <nav aria-label={t(messages['header.label.main.nav'])} className="kkux-header__nav kkux-header__nav--main">
+          <DesktopMainMenuSlot menu={mainMenu} />
+        </nav>
 
-  renderLoggedOutItems() {
-    const { loggedOutItems } = this.props;
-    return <DesktopLoggedOutItemsSlot items={loggedOutItems} />;
-  }
+        {/* Right side: secondary menu + user */}
+        <nav aria-label={t(messages['header.label.secondary.nav'])} className="kkux-header__nav kkux-header__nav--right">
+          {loggedIn ? (
+            <>
+              <DesktopSecondaryMenuSlot menu={secondaryMenu} />
+              <Menu transitionClassName="menu-dropdown" transitionTimeout={250}>
+                <MenuTrigger
+                  tag="button"
+                  aria-label={t(messages['header.label.account.menu.for'], { username })}
+                >
+                  <svg width="20" height="20" viewBox="0 0 24 24" fill="none" aria-hidden="true" className="kkux-header__user-icon">
+                    <circle cx="12" cy="8" r="4" fill="currentColor" />
+                    <path d="M4 20c0-4 4-6 8-6s8 2 8 6" stroke="currentColor" strokeWidth="2" fill="none" />
+                  </svg>
+                  <span className="kkux-header__username">{username}</span>
+                  <svg width="12" height="12" viewBox="0 0 16 16" aria-hidden="true" className="kkux-header__chevron">
+                    <path d="M4 6l4 4 4-4" stroke="currentColor" strokeWidth="1.5" fill="none" strokeLinecap="round" strokeLinejoin="round" />
+                  </svg>
+                </MenuTrigger>
+                <MenuContent className="kkux-header__dropdown">
+                  <DesktopUserMenuSlot menu={userMenu} />
+                </MenuContent>
+              </Menu>
+            </>
+          ) : (
+            <DesktopLoggedOutItemsSlot items={loggedOutItems} />
+          )}
+        </nav>
+      </div>
+    </header>
+  );
+};
 
-  render() {
-    const {
-      logo,
-      logoAltText,
-      logoDestination,
-      loggedIn,
-      intl,
-    } = this.props;
-    const logoProps = { src: logo, alt: logoAltText, href: logoDestination };
-    const logoClasses = getConfig().AUTHN_MINIMAL_HEADER ? 'mw-100' : null;
-
-    return (
-      <header className="site-header-desktop">
-        <a className="nav-skip sr-only sr-only-focusable" href="#main">{intl.formatMessage(messages['header.label.skip.nav'])}</a>
-        <div className={`container-fluid ${logoClasses}`}>
-          <div className="nav-container position-relative d-flex align-items-center">
-            <LogoSlot {...logoProps} />
-            <nav
-              aria-label={intl.formatMessage(messages['header.label.main.nav'])}
-              className="nav main-nav"
-            >
-              {this.renderMainMenu()}
-            </nav>
-            <nav
-              aria-label={intl.formatMessage(messages['header.label.secondary.nav'])}
-              className="nav secondary-menu-container align-items-center ml-auto"
-            >
-              {loggedIn
-                ? (
-                  <>
-                    {this.renderSecondaryMenu()}
-                    {this.renderUserMenu()}
-                  </>
-                ) : this.renderLoggedOutItems()}
-            </nav>
-          </div>
-        </div>
-      </header>
-    );
-  }
-}
-
-export const desktopHeaderDataShape = {
+DesktopHeader.propTypes = {
   mainMenu: desktopHeaderMainOrSecondaryMenuDataShape,
   secondaryMenu: desktopHeaderMainOrSecondaryMenuDataShape,
   userMenu: desktopUserMenuDataShape,
@@ -119,22 +99,6 @@ export const desktopHeaderDataShape = {
   avatar: PropTypes.string,
   username: PropTypes.string,
   loggedIn: PropTypes.bool,
-};
-
-DesktopHeader.propTypes = {
-  mainMenu: desktopHeaderDataShape.mainMenu,
-  secondaryMenu: desktopHeaderDataShape.secondaryMenumainMenu,
-  userMenu: desktopHeaderDataShape.userMenumainMenu,
-  loggedOutItems: desktopHeaderDataShape.loggedOutItemsmainMenu,
-  logo: desktopHeaderDataShape.logomainMenu,
-  logoAltText: desktopHeaderDataShape.logoAltTextmainMenu,
-  logoDestination: desktopHeaderDataShape.logoDestinationmainMenu,
-  avatar: desktopHeaderDataShape.avatarmainMenu,
-  username: desktopHeaderDataShape.usernamemainMenu,
-  loggedIn: desktopHeaderDataShape.loggedInmainMenu,
-
-  // i18n
-  intl: intlShape.isRequired,
 };
 
 DesktopHeader.defaultProps = {
@@ -150,4 +114,4 @@ DesktopHeader.defaultProps = {
   loggedIn: false,
 };
 
-export default injectIntl(DesktopHeader);
+export default DesktopHeader;
